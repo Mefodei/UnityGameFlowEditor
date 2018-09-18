c74a84605db8138b59853891efbe89326b159f40
Mihaylov Sergey <sergey.mihaylov@zimad.com>
2018-09-18 18:18:05 +0300

    - add uniStatebehaviour

---
+++ UnityNodeEditor/Assets/GameGraph/Nodes/GameStateNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace GameGraphModule
+{
+	[Serializable]
+	public class GameStateNode : Node
+	{
+
+		
+		
+	}
+}
---
+++ UnityNodeEditor/Assets/GameGraph/Scripts/UniStateBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Tools.StateMachine;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameGraph/GameState",fileName = "GameState")]
+public class UniStateBehaviour : ScriptableObject, IStateBehaviour<IEnumerator> {
+	
+	
+	public IEnumerator Execute()
+	{
+		yield break;
+	}
+
+	public void Stop()
+	{
+		
+	}
+
+	public void Dispose()
+	{
+		
+	}
+}
